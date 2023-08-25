d6d348bb30a47748b73fa3373b225780adf6e5fc
Rick Hallett <rick.hallett@brandwatch.com>
2023-08-25 08:09:23 +0100

    fix: update Resend new domain

--- app/utils/email.server.ts
+++ app/utils/email.server.ts
@@ -31,7 +31,7 @@ export async function sendEmail({
 	| { html: string; text: string; react?: never }
 	| { react: ReactElement; html?: never; text?: never }
 )) {
-	const from = 'storyswap.example.com'
+	const from = 'email.storyswap.com'
 
 	const email = {
 		from,
