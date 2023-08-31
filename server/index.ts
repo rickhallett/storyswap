6a922e2acacfd70188bd04e05bb8f2b004979873
Rick Hallett <rick.hallett@brandwatch.com>
2023-08-31 01:20:24 +0100

    fix: prevent image self server policy

--- server/index.ts
+++ server/index.ts
@@ -113,7 +113,7 @@ app.use(
 				].filter(Boolean),
 				'font-src': ["'self'"],
 				'frame-src': ["'self'"],
-				'img-src': ["'self'", 'data:'],
+				// 'img-src': ["'self'", 'data:'],
 				'script-src': [
 					"'strict-dynamic'",
 					"'self'",
